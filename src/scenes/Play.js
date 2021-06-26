e87d5d6c390a0f80efaa391c2834c9a14d07215e
Qiuting Zhao <qzhao29@ucsc.edu>
2021-06-26 12:05:48 -0700

    sound

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -61,6 +61,10 @@ class Play extends Phaser.Scene {
         }, null, this);
     }
     update() {
+        // check key input for restart
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.restart();
+        }
         this.starfield.tilePositionX -= 4;
         if (!this.gameOver) {
             this.p1Rocket.update();
@@ -100,5 +104,6 @@ class Play extends Phaser.Scene {
         });
         this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
+        this.sound.play('sfx_explosion');
     }
 }
